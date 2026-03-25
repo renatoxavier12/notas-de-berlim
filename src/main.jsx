fb9c037ebc0021b34ac7bd4493a9091930df2960
Renato Xavier <renatoxavier@Mac.fritz.box>
2026-03-26 00:25:25 +0100

    fix vercel analytics component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
-import { inject } from '@vercel/analytics';
-inject();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
