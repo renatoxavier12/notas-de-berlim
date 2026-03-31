689e20e07ae5a1e0e5d4fee7db4e51eef4a35a9e
Renato Xavier <renatoxavier12@gmail.com>
2026-03-31 23:35:26 +0200

    Fix home share metadata and copy

--- src/lib/site.js
+++ src/lib/site.js
@@ -1,6 +1,6 @@
 export const SITE_URL = 'https://notasdeberlim.com'
 export const SITE_NAME = 'Notas de Berlim'
-export const SITE_DESCRIPTION = 'Comida, drink, Kultur. Vida em Berlim.'
+export const SITE_DESCRIPTION = 'Ensaios sobre a vida em Berlim.'
 export const DEFAULT_OG_IMAGE = `${SITE_URL}/og.jpg`
 
 const PT_MONTHS = {
